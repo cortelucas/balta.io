5131462a4fddb5cef78582d7e3b371f6225ef0df
Lucas S. Corte <corte1994.lc@gmail.com>
2023-08-29 10:17:57 -0300

    feat: implementation menu

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,11 +6,45 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Insira o Primeiro valor: ");
-            float firstNumber = float.Parse(Console.ReadLine() ?? "Não inseriu número");
+            Menu();
+        }
 
-            Console.WriteLine(firstNumber);
+        static void Menu ()
+        {
+            Console.Clear();
+            Console.WriteLine("------ Calculator -----");
+            Console.WriteLine("--------- MENU --------");
+            Console.WriteLine("| Adição ........ (+) |");
+            Console.WriteLine("| Subtração ..... (-) |");
+            Console.WriteLine("| Multiplicação . (*) |");
+            Console.WriteLine("| Divisão ....... (/) |");
+            Console.WriteLine("| Sair .......... (0) |");
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("");
+            Console.WriteLine("Insira a Sua Escolha: ");
+            string choice = (Console.ReadLine() ?? "0").ToString();
+
+            switch (choice)
+            {
+                case "+":
+                    Console.WriteLine("Insira os números, separados por ',': ");
+                    break;
+                case "-":
+                    Console.WriteLine("Insira os números, separados por ',': ");
+                    break;
+                case "*":
+                    Console.WriteLine("Insira os números, separados por ',': ");
+                    break;
+                case "/":
+                    Console.WriteLine("Insira os números, separados por ',': ");
+                    break;
+                case "0":
+                    Console.WriteLine("Obrigado por usar Calculator");
+                    break;
+                default:
+                    Console.WriteLine("Você não digitou uma opção válida");
+                    break;
+            }
         }
     }
 }
