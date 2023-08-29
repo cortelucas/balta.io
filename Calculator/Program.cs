4c64bcdf2c03a1b8aff63f4768039a941147c745
Lucas S. Corte <corte1994.lc@gmail.com>
2023-08-29 08:46:11 -0300

    chore: init project

---
+++ Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Insira o Primeiro valor: ");
+            float firstNumber = float.Parse(Console.ReadLine() ?? "Não inseriu número");
+
+            Console.WriteLine(firstNumber);
+        }
+    }
+}
